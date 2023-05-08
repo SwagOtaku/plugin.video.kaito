c8c4115ade216fa6e678ba2bf18b99b621c8a66e
Goldenfreddy0703 <62456796+Goldenfreddy0703@users.noreply.github.com>
2023-05-08 05:14:09 -0400

    Updated Version
    Changelog 0.3.70 [20230508]:
    - Added playtaku embed extractor
    - Added tmdb2anilist.py
    - Made Animesaturn invisible for the time being.

---
+++ plugin.video.otaku/resources/lib/indexers/tmdb2anilist.py
@@ -0,0 +1,34 @@
+import json
+
+from resources.lib.ui import client
+
+
+def get_tmdb_to_anilist_id_tv(tmdb_id, seasonnum):
+    # Convert Tmdb ID to Anilist ID
+
+    url = 'https://tmdb2anilist.slidemovies.org/tv/?id=%s&s=%s' % (tmdb_id, seasonnum)
+    params = {
+        'tmdb_id': 0,
+        'anilist_id': 0,
+        'mal_id': 0
+    }
+
+    r = client.request(url, params=params)
+    if r:
+        r = json.loads(r)
+    return r
+
+def get_tmdb_to_anilist_id_movie(tmdb_id):
+    # Convert Tmdb ID to Anilist ID
+
+    url = 'https://tmdb2anilist.slidemovies.org/movie/?id=%s' % (tmdb_id)
+    params = {
+        'tmdb_id': 0,
+        'anilist_id': 0,
+        'mal_id': 0
+    }
+
+    r = client.request(url, params=params)
+    if r:
+        r = json.loads(r)
+    return r
